6b72a7afe58eaf4cfc3f4da4553cdbcc704b46f9
Andrew Stuart <andrew.stuart2@gmail.com>
2023-04-02 13:47:18 -0700

    P.F

--- p.go
+++ p.go
@@ -1,5 +1,13 @@
 package p
 
+import "fmt"
+
+// Return a pointer to anything
 func T[T any](t T) *T {
 	return &t
+}
+
+// Return a pointer to a printf'd string
+func F(tpl string, args ...any) *string {
+	return T(fmt.Sprintf(tpl, args...))
 }
